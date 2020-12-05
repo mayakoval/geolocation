54932b24cef0405a623e146c09054803d2eb9269
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-12-05 23:59:47 +0000

    Routes and controllers generated

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,36 @@
+class AppointmentsController < ApplicationController
+	before_action :set_doctor, except: [:index, :show, :destroy]
+
+	def new
+		@appointment = Appointment.new
+		@doctor = set_doctor
+	end
+
+	def index
+		@appointments = Appointment.all
+	end
+
+	def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+	def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.doctor = @doctor
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new
+    end
+  end
+
+	private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:doctor_id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:user_name, :email, :date, :time, :nhs_number)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http s://guides.rubyonrails.org/routing.html
+  resources :doctors, only: [:index, :show, :new] do
+    resources :appointments, only: [:new, :create, :show]
+  end
+  resources :appointments, only: [:destroy]
+
 end
