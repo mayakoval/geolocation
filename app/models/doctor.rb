ea15c5ec55027a97c4607db25891c1f66b0ec7b3
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-12-06 14:51:49 +0000

    Geocoding added to Doctor class

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,6 @@ class Doctor < ApplicationRecord
 	has_many :appointments, dependent: :destroy
 
 	validates :forename, :surname, :specialty, :address, :description, presence: true
+	geocoded_by :address
+	after_validation :geocode, if: :will_save_change_to_address?
 end
